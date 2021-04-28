034a51cc35d5e0a14bdcef28d5a4bdabbd5c0678
Fasasi Qasim <fasaqa1000@gmail.com>
2021-04-28 03:59:09 +0100

    added a media query for the nav menu

---
+++ app.js
@@ -0,0 +1,11 @@
+let menuItem = document.getElementById('menuItem')
+
+menuItem.style.maxHeight = "0px";
+
+function menutoggle(){
+    if(menuItem.style.maxHeight == "0px"){
+        menuItem.style.maxHeight ="200px"
+    }else{
+        menuItem.style.maxHeight="0px"
+    }
+}
